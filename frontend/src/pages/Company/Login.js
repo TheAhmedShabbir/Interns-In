f2176145b829ef52a7cc68ecd295c94f88e2a230
XboyAbdullah <67580804+XboyAbdullah@users.noreply.github.com>
2022-01-26 20:16:49 +0500

    company_initial_commit

---
+++ frontend/src/pages/Company/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class CompanyLogin extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default CompanyLogin;
---
+++ frontend/src/pages/Company/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function CompanySignUp() {
+  return <div>
+      <h1>Sign up Page for company users</h1>
+  </div>;
+}
+export default CompanySignUp;
