5997dbe792a00c64a4d9cbdfd2bf83943ab47081
oyeahmed <ahmed.shabbir1308@gmail.com>
2022-01-27 20:45:36 +0500

    login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { Component } from 'react';
-import Login from "./pages/User/Login";
+import Login from "./pages/Login";
 import SignUp from "./pages/User/SignUp";
 import './App.css';
 
