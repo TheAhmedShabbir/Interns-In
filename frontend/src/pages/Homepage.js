c2456fea3c76514d6be833b4a36b473bbeb237c8
XboyAbdullah <67580804+XboyAbdullah@users.noreply.github.com>
2022-01-31 19:48:42 +0500

    working on homepage

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Generalheader from '../Components/General/header';
+
+
+export default function Homepage() {
+  return <div>
+    <Generalheader/>
+    <div>
+      <img src="../assets/images/homepage-image.jpg" alt=""/>
+    </div>
+      <h1>This is home page</h1>
+  </div>;
+}
