db9bd22d87d38b36584aa2c242e485fb7107a95c
oyeahmed <ahmed.shabbir1308@gmail.com>
2022-02-22 14:21:48 +0500

    postjob collection

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// initialize services
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
+
+// collection references
+
+const colref = collection(db, "postjob");
+
+// get collection data
+getDocs(colref)
+  .then((ss) => {
+    let postjob = [];
+    ss.docs.forEach((doc) => {
+      postjob.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(postjob);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
