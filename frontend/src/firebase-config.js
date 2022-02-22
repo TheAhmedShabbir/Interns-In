79759fde8e4b37eb2c2414985cbb3ac233344578
oyeahmed <ahmed.shabbir1308@gmail.com>
2022-02-22 13:36:25 +0500

    firebase install

---
+++ frontend/src/firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByhxz1wYGP-1fUZ0v2T45q0vPHfW_LLpY",
+  authDomain: "interns-in-f1f1e.firebaseapp.com",
+  projectId: "interns-in-f1f1e",
+  storageBucket: "interns-in-f1f1e.appspot.com",
+  messagingSenderId: "850988262906",
+  appId: "1:850988262906:web:39c68b09413369188a3043",
+  measurementId: "G-5BE8S16SYQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
