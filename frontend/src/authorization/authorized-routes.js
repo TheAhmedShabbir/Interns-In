0704713f86d4ecce0afdde904b9f5460ab288a4c
oyeahmed <ahmed.shabbir1308@gmail.com>
2022-05-28 20:00:26 +0500

    shortlisted done

--- frontend/src/authorization/authorized-routes.js
+++ frontend/src/authorization/authorized-routes.js
@@ -1,16 +1,15 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
 
 export const AuthorizedRoute = ({ children }) => {
-  if (!localStorage.getItem('access_token')) {
+  if (!localStorage.getItem("access_token")) {
     return <Navigate to="/signin" replace />;
   }
   return children ? children : <Outlet />;
 };
 
 export const EnrouteToDashboard = ({ children }) => {
-  if (localStorage.getItem('access_token')) {
+  if (localStorage.getItem("access_token")) {
     return <Navigate to={`/${localStorage.getItem("page")}`} replace />;
   }
   return children ? children : <Outlet />;
 };
-
