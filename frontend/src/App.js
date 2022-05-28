ab4f5f2583235065621bb5a18712d3c616310cf9
oyeahmed <ahmed.shabbir1308@gmail.com>
2022-05-28 19:19:19 +0500

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,9 +35,30 @@ export default function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<EnrouteToDashboard><Mainpage /></EnrouteToDashboard>}></Route>
-          <Route path="/SignIn" element={<EnrouteToDashboard><SignIn /></EnrouteToDashboard>}></Route>
-          <Route path="/UserSignUp" element={<EnrouteToDashboard><UserSignUp /></EnrouteToDashboard>}></Route>
+          <Route
+            path="/"
+            element={
+              <EnrouteToDashboard>
+                <Mainpage />
+              </EnrouteToDashboard>
+            }
+          ></Route>
+          <Route
+            path="/SignIn"
+            element={
+              <EnrouteToDashboard>
+                <SignIn />
+              </EnrouteToDashboard>
+            }
+          ></Route>
+          <Route
+            path="/UserSignUp"
+            element={
+              <EnrouteToDashboard>
+                <UserSignUp />
+              </EnrouteToDashboard>
+            }
+          ></Route>
           <Route path="/CompanySignUp" element={<CompanySignUp />}></Route>
           <Route path="/AdminProfile" element={<AdminProfile />}></Route>
           <Route
